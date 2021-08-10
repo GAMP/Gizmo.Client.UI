094d0c611af1c9533324dfb681e8edc638aab6ff
Mukesh <mukeshsalaria01@gmail.com>
2021-08-10 17:26:33 +0530

    Client UI Cleanup

--- Gizmo.Client.UI.Host.Web/Program.cs
+++ Gizmo.Client.UI.Host.Web/Program.cs
@@ -18,36 +18,6 @@ namespace Gizmo.Client.UI.Host.Web
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            ////create services variable
-            //var services = builder.Services;
-            //#region VIEW MODELS
-
-            ////add by type
-            //foreach (var type in Assembly.GetExecutingAssembly().ExportedTypes)
-            //{
-            //    //get type info
-            //    var typeInfo = type.GetTypeInfo();
-
-            //    //must be a class
-            //    if (!typeInfo.IsClass)
-            //        continue;
-
-            //    //must be non abstract
-            //    if (typeInfo.IsAbstract)
-            //        continue;
-
-            //    //attributes can be checked here for client exclusion e.t.c
-
-            //    //any type that implement IPageViewModel added as singelton service
-            //    if (typeInfo.GetInterfaces().Any(t => t == typeof(IPageViewModel)))
-            //        services.AddSingleton(type);
-
-            //    //any type that implement IComponentViewModel added as singelton service
-            //    if (typeInfo.GetInterfaces().Any(t => t == typeof(IComponentViewModel)))
-            //        services.AddSingleton(type);
-            //}
-
-            //#endregion
 
             builder.RootComponents.Add<App>("#app");
 
