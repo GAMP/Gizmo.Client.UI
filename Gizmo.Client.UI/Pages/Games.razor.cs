9377cc3362e653945b06309c90bc6bd95782881d
Mukesh <mukeshsalaria01@gmail.com>
2021-08-11 11:02:18 +0530

    Client UI Nav links

---
+++ Gizmo.Client.UI/Pages/Games.razor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gizmo.Client.UI.Pages
+{
+    public partial class Games : ComponentBase
+    {
+    }
+}
