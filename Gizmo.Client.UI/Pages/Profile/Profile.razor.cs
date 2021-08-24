8be072d538c5fa386a7da068eac15f531bc5b5d1
Mukesh <mukeshsalaria01@gmail.com>
2021-08-24 11:01:10 +0530

    Client Ui User detail page

--- Gizmo.Client.UI/Pages/Profile/Profile.razor.cs
+++ Gizmo.Client.UI/Pages/Profile/Profile.razor.cs
@@ -4,7 +4,7 @@ namespace Gizmo.Client.UI.Pages
 {
     [ModuleGuid(KnownModules.MODULE_PROFILE)]
     [PageUIModule(TitleLocalizationKey = "MODULE_PAGE_PROFILE_TITLE", DescriptionLocalizationKey = "MODULE_PAGE_PROFILE_TITLE"), ModuleDisplayOrder(3)]
-    [DefaultRoute("/profile", DefaultRouteMatch = NavlinkMatch.Prefix)]
+   // [DefaultRoute("/profile", DefaultRouteMatch = NavlinkMatch.Prefix)]
     [Route("/profile")]
     public partial class Profile : ComponentBase
     {
