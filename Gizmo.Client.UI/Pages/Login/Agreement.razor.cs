a7c33527d87b8b2c085dc04d0cddb60f46266c2b
Mukesh <mukeshsalaria01@gmail.com>
2021-09-06 12:18:56 +0530

    Login UI layout

---
+++ Gizmo.Client.UI/Pages/Login/Agreement.razor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Gizmo.Client.UI.Pages
+{
+    [Route("/Agreement")]
+    public partial class Agreement : ComponentBase
+    {
+    }
+}
---
+++ Gizmo.Client.UI/Pages/Login/Loading.razor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Gizmo.Client.UI.Pages
+{
+    [Route("/Loading")]
+    public partial class Loading : ComponentBase
+    {
+    }
+}
