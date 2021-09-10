3edf5dae409633a1886d244e49d1ac9d7bdd60e2
klapsis-netprojects <72749987+klapsis-netprojects@users.noreply.github.com>
2021-09-10 13:39:22 +0300

    Update shop page components.

---
+++ Gizmo.Client.UI/Shared/GizShopCard.razor.cs
@@ -0,0 +1,20 @@
+﻿using Gizmo.Client.UI.ViewModels;
+using Microsoft.AspNetCore.Components;
+using System.Threading.Tasks;
+
+namespace Gizmo.Client.UI.Shared
+{
+    public partial class GizShopCard
+    {
+        [Parameter]
+        public ProductViewModel Product { get; set; }
+
+        [Parameter]
+        public EventCallback<int> OnAddProduct { get; set; }
+
+        public async Task AddProduct()
+        {
+            await OnAddProduct.InvokeAsync(Product.Id);
+        }
+    }
+}
