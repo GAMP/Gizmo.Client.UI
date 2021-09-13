e8f2aba708134f6957c32b8b0658f38cf9c85854
Mukesh <mukeshsalaria01@gmail.com>
2021-09-13 12:13:50 +0530

    Client UI confirm phone number

---
+++ Gizmo.Client.UI/Pages/Login/ConfirmPhoneNumber.razor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Gizmo.Client.UI.Pages
+{
+    [Route("/confirmphonenumber")]
+    public partial class ConfirmPhoneNumber : ComponentBase
+    {
+    }
+}
