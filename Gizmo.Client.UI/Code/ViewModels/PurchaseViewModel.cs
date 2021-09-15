0b3f81c16575e3d7137a9b28d0363b8e9643c8f8
klapsis-netprojects <72749987+klapsis-netprojects@users.noreply.github.com>
2021-09-15 11:35:45 +0300

    Add DataGrid in user history.

---
+++ Gizmo.Client.UI/Code/ViewModels/PurchaseViewModel.cs
@@ -0,0 +1,33 @@
+﻿using Gizmo.Shared.ViewModels;
+using System;
+
+namespace Gizmo.Client.UI.ViewModels
+{
+    public class PurchaseViewModel : ViewModelBase
+    {
+        public string ProductName
+        {
+            get; set;
+        }
+
+        public int Quantity
+        {
+            get; set;
+        }
+
+        public DateTime Date
+        {
+            get; set;
+        }
+
+        public decimal Total
+        {
+            get; set;
+        }
+
+        public string PaymentMethod
+        {
+            get; set;
+        }
+    }
+}
