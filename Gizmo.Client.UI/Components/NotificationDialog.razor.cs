34becd36f338864a579c835df9144abf4bd92453
klapsis-netprojects <72749987+klapsis-netprojects@users.noreply.github.com>
2021-09-21 13:09:42 +0300

    Fix for multiple notifications.

---
+++ Gizmo.Client.UI/Components/NotificationDialog.razor.cs
@@ -0,0 +1,21 @@
+﻿using Gizmo.Client.UI.ViewModels;
+using Gizmo.Web.Components;
+using Microsoft.AspNetCore.Components;
+using System.Threading.Tasks;
+
+namespace Gizmo.Client.UI.Components
+{
+    public partial class NotificationDialog : CustomDOMComponentBase
+    {
+        [Parameter]
+        public EventCallback<int> OnCloseDialog { get; set; }
+
+        [Parameter]
+        public NotificationViewModel Notification { get; set; }
+
+        public async Task CloseDialog()
+        {
+            await OnCloseDialog.InvokeAsync(Notification.Id);
+        }
+    }
+}
