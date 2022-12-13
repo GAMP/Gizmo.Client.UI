dc416cb9e9e9961365ce6a0a406efe8d10c88633
klapsis-netprojects <klapsis@netprojects.gr>
2022-12-13 17:55:05 +0200

    Update carousel.

--- Gizmo.Client.UI/Components/Common/AdsCarouselIndicator.razor.cs
+++ Gizmo.Client.UI/Components/Common/AdsCarouselIndicator.razor.cs
@@ -1,14 +1,19 @@
 ﻿using Gizmo.Web.Components;
 using Microsoft.AspNetCore.Components;
+using System;
 
 namespace Gizmo.Client.UI.Components
 {
     public partial class AdsCarouselIndicator : CustomDOMComponentBase
     {
         private int _selectedIndex;
+        private int _direction;
 
         [Parameter]
         public int Size { get; set; }
+
+        [Parameter]
+        public int Maximum { get; set; }
 
         [Parameter]
         public int SelectedIndex
@@ -22,6 +27,18 @@ namespace Gizmo.Client.UI.Components
                 if (_selectedIndex == value)
                     return;
 
+                if (Math.Abs(_selectedIndex - value) == 1)
+                {
+                    if (_selectedIndex > value)
+                        _direction = 1;
+                    else
+                        _direction = -1;
+                }
+                else
+                {
+                    _direction = 0;
+                }
+
                 _selectedIndex = value;
                 _ = SelectedIndexChanged.InvokeAsync(_selectedIndex);
             }
@@ -33,6 +50,77 @@ namespace Gizmo.Client.UI.Components
         public void OnClickButton(int index)
         {
             SelectedIndex = index;
+        }
+
+        private string GetIndicatorClass(int index)
+        {
+            string result = string.Empty;
+
+            if (index == SelectedIndex)
+            {
+                result = "active";
+            }
+
+            if (_direction == 0)
+                return result;
+
+            if (index == SelectedIndex)
+            {
+                if (_direction < 0)
+                    result += " current";
+                else
+                    result += " next";
+            }
+
+            if (index == SelectedIndex + _direction)
+            {
+                if (_direction < 0)
+                    result = "next";
+                else
+                    result = "current";
+            }
+
+            return result;
+        }
+
+        private const decimal INDICATOR_BORDER = 2;
+        private const decimal INDICATOR_DIMENSIONS = 16;
+        private const decimal SPACE_BETWEEN = 16;
+
+        private decimal GetIndicatorListSize()
+        {
+            return ((Size * INDICATOR_DIMENSIONS) + ((Size - 1) * SPACE_BETWEEN) + (INDICATOR_BORDER * 2)) / 10;
+        }
+
+        private decimal GetIndicatorPosition(int index)
+        {
+            decimal result = INDICATOR_BORDER;
+
+            if (_direction != 0)
+            {
+                if (index == SelectedIndex)
+                {
+                    if (_direction < 0)
+                        index -= 1;
+                    else
+                        index += 1;
+                }
+                else
+                {
+                    if (index == SelectedIndex + _direction)
+                    {
+                        if (_direction < 0)
+                            index += 1;
+                        else
+                            index -= 1;
+                    }
+                }
+            }
+
+            result += (index + 1) * INDICATOR_DIMENSIONS / 2;
+            result += index * SPACE_BETWEEN;
+
+            return result / 10;
         }
     }
 }
