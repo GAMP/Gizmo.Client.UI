f7abf7b3c937477094631b683df531ec07f0bbc3
Andrey Pestunov <andreypestunov@gmail.com>
2023-02-27 18:54:34 +0100

    refactor: refactor shopindex

--- Gizmo.Client.UI/Pages/Shop/ShopIndex.razor.cs
+++ Gizmo.Client.UI/Pages/Shop/ShopIndex.razor.cs
@@ -1,15 +1,13 @@
-﻿using Gizmo.Client.UI.View.Services;
+﻿using System.Linq;
+using System.Threading.Tasks;
+
+using Gizmo.Client.UI.View.Services;
 using Gizmo.Client.UI.View.States;
-using Gizmo.Client.UI.ViewModels;
 using Gizmo.UI;
 using Gizmo.UI.Services;
 using Gizmo.Web.Components;
+
 using Microsoft.AspNetCore.Components;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Input;
 
 namespace Gizmo.Client.UI.Pages
 {
@@ -18,14 +16,6 @@ namespace Gizmo.Client.UI.Pages
     [Route(ClientRoutes.ShopRoute)]
     public partial class ShopIndex : CustomDOMComponentBase
     {
-        public ShopIndex()
-        {
-        }
-
-        #region FIELDS
-        private List<string> _sortOptions;
-        #endregion
-
         #region PROPERTIES
 
         [Inject]
@@ -40,34 +30,13 @@ namespace Gizmo.Client.UI.Pages
         [Inject]
         SearchService SearchService { get; set; }
 
-        public string SelectedSortOption { get; set; } = "Name";
-
-        public List<string> SortOptions
-        {
-            get
-            {
-                if (_sortOptions == null)
-                {
-                    _sortOptions = new List<string>();
-                    _sortOptions.Add("Name");
-                    _sortOptions.Add("Price");
-                }
-
-                return _sortOptions;
-            }
-            set
-            {
-                _sortOptions = value;
-            }
-        }
-
         #endregion
 
         #region METHODS
 
-        public IEnumerable<ProductViewState> GetFilteredProducts()
+        private ProductViewState[] GetFilteredProducts()
         {
-            var result = ShopService.ViewState.Products.AsQueryable();
+            var result = ShopService.ViewState.Products;
 
             if (SearchService.ViewState.ShowAll)
             {
@@ -75,12 +44,13 @@ namespace Gizmo.Client.UI.Pages
 
                 result = result.Where(a => ids.Contains(a.Id));
             }
-            else if (ShopService.ViewState.SelectedUserProductGroupId.HasValue)
+
+            if (ShopService.ViewState.SelectedUserProductGroupId.HasValue)
             {
                 result = result.Where(a => a.ProductGroupId == ShopService.ViewState.SelectedUserProductGroupId.Value);
             }
 
-            return result;
+            return result.ToArray();
         }
 
         #endregion
@@ -89,8 +59,6 @@ namespace Gizmo.Client.UI.Pages
         {
             this.SubscribeChange(ShopService.ViewState);
             this.SubscribeChange(SearchService.ViewState);
-
-            //await ShopService.LoadProductsAsync();
 
             await base.OnInitializedAsync();
         }
