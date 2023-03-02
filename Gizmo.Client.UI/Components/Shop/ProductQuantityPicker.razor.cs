c36f89abfbc0f388b6fde8aa3b02be533bdb86bc
klapsis-netprojects <klapsis@netprojects.gr>
2023-03-02 14:54:08 +0200

    Fixes for click and sync.

--- Gizmo.Client.UI/Components/Shop/ProductQuantityPicker.razor.cs
+++ Gizmo.Client.UI/Components/Shop/ProductQuantityPicker.razor.cs
@@ -2,6 +2,7 @@
 
 using Gizmo.Client.UI.View.Services;
 using Gizmo.Client.UI.View.States;
+using Gizmo.UI;
 using Gizmo.UI.Services;
 using Gizmo.Web.Components;
 
@@ -12,6 +13,8 @@ namespace Gizmo.Client.UI.Components
 {
     public partial class ProductQuantityPicker : CustomDOMComponentBase
     {
+        private int _previousProductId;
+
         [Inject]
         ILocalizationService LocalizationService { get; set; }
 
@@ -24,19 +27,52 @@ namespace Gizmo.Client.UI.Components
         [Parameter]
         public ButtonSizes PikerSize { get; set; } = ButtonSizes.Medium;
 
+        [Parameter]
+        public EventCallback<MouseEventArgs> OnClick { get; set; }
+
         UserCartProductItemViewState ProductItemViewState { get; set; }
 
-        public Task OnAddQuantityButtonClickHandlerAsync(MouseEventArgs _) => 
-            UserCartService.AddProductAsync(ProductId, 1);
-        public Task OnRemoveQuantityButtonClickHandler(MouseEventArgs _) => 
-            UserCartService.RemoveProductAsync(ProductId, 1);
-
-        protected override async Task OnInitializedAsync()
+        public async Task OnAddQuantityButtonClickHandlerAsync(MouseEventArgs args)
         {
-            ProductItemViewState = await UserCartService.GetCartProductItemViewStateAsync(ProductId);
+            await UserCartService.AddProductAsync(ProductId, 1);
+            await OnClick.InvokeAsync(args);
+        }
 
-            this.SubscribeChange(ProductItemViewState);
-            base.OnInitialized();
+        public async Task OnRemoveQuantityButtonClickHandler(MouseEventArgs args)
+        {
+            await UserCartService.RemoveProductAsync(ProductId, 1);
+            await OnClick.InvokeAsync(args);
+        }
+
+        public override async Task SetParametersAsync(ParameterView parameters)
+        {
+            await base.SetParametersAsync(parameters);
+
+            var productIdChanged = _previousProductId != ProductId;
+
+            if (productIdChanged)
+            {
+                _previousProductId = ProductId;
+
+                if (ProductItemViewState != null)
+                {
+                    //The same component used again with a different product id.
+                    //We have to unbind from the old product.
+                    this.UnsubscribeChange(ProductItemViewState);
+                }
+
+                //We have to bind to the new product.
+                ProductItemViewState = await UserCartService.GetCartProductItemViewStateAsync(ProductId);
+                this.SubscribeChange(ProductItemViewState);
+            }
+        }
+
+        protected override Task OnInitializedAsync()
+        {
+            //ProductItemViewState = await UserCartService.GetCartProductItemViewStateAsync(ProductId);
+
+            //this.SubscribeChange(ProductItemViewState);
+            return base.OnInitializedAsync();
         }
         public override void Dispose()
         {
