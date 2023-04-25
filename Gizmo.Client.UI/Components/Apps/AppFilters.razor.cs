477db84d82227423bf33a9c381ce390e7608681b
klapsis-netprojects <klapsis@netprojects.gr>
2023-04-25 13:29:22 +0300

    App filters sticky header.

---
+++ Gizmo.Client.UI/Components/Apps/AppFilters.razor.cs
@@ -0,0 +1,38 @@
+﻿using Gizmo.Client.UI.View.Services;
+using Gizmo.Client.UI.View.States;
+using Gizmo.UI.Services;
+using Gizmo.Web.Components;
+using Microsoft.AspNetCore.Components;
+
+namespace Gizmo.Client.UI.Components
+{
+    public partial class AppFilters : CustomDOMComponentBase
+    {
+        #region PROPERTIES
+
+        [Inject]
+        ILocalizationService LocalizationService { get; set; }
+
+        [Inject]
+        AppsPageViewService AppsPageService { get; set; }
+
+        [Inject()]
+        public AppsPageViewState ViewState { get; set; }
+
+        #endregion
+
+        protected override void OnInitialized()
+        {
+            this.SubscribeChange(ViewState);
+
+            base.OnInitialized();
+        }
+
+        public override void Dispose()
+        {
+            this.UnsubscribeChange(ViewState);
+
+            base.Dispose();
+        }
+    }
+}
