f8710f04cb1830c78f7635744c0e175f471e8b60
klapsis-netprojects <klapsis@netprojects.gr>
2023-05-18 10:18:01 +0300

    Pause rss.

--- Gizmo.Client.UI/Components/Common/NewsRotator.razor.cs
+++ Gizmo.Client.UI/Components/Common/NewsRotator.razor.cs
@@ -1,26 +1,42 @@
-﻿using Gizmo.Client.UI.View.States;
+﻿using System.Threading.Tasks;
+using Gizmo.Client.UI.View.States;
+using Gizmo.Client.View.Services;
 using Gizmo.Web.Components;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 
 namespace Gizmo.Client.UI.Components
 {
     public partial class NewsRotator : CustomDOMComponentBase
     {
         [Inject]
-        public FeedsViewState FeedsViewState { get; set; }
+        public FeedsViewService FeedsViewService { get; set; }
+
+        [Inject]
+        public FeedsViewState ViewState { get; set; }
+
+        private Task OnMouseOverHandler(MouseEventArgs args)
+        {
+            return FeedsViewService.PauseAsync(true);
+        }
+
+        private Task OnMouseOutHandler(MouseEventArgs args)
+        {
+            return FeedsViewService.PauseAsync(false);
+        }
 
         #region OVERRIDES
 
         protected override void OnInitialized()
         {
-            this.SubscribeChange(FeedsViewState);
+            this.SubscribeChange(ViewState);
 
             base.OnInitialized();
         }
 
         public override void Dispose()
         {
-            this.UnsubscribeChange(FeedsViewState);
+            this.UnsubscribeChange(ViewState);
 
             base.Dispose();
         }
