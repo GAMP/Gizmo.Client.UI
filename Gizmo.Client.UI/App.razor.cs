53b953836433518d8d5154563b6fd1552739ad97
Oleg Savelos <olegsavelos@gmail.com>
2023-05-19 16:23:03 +0300

    JSRuntime fix.

--- Gizmo.Client.UI/App.razor.cs
+++ Gizmo.Client.UI/App.razor.cs
@@ -2,6 +2,7 @@
 using Gizmo.Client.UI.Services;
 using Gizmo.UI.Services;
 using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 
 namespace Gizmo.Client.UI;
 
@@ -14,13 +15,18 @@ public partial class App : ComponentBase
     /// </summary>
     [Inject] public IUICompositionService ComponentDiscoveryService { get; protected set; }
     [Inject] private NavigationManager NavigationManager { get; set; }
+    [Inject] private IJSRuntime JSRuntime { get; set; }
+    [Inject] private JSRuntimeService JSRuntimeService { get; set; }
     [Inject] private NavigationService NavigationService { get; set; }
     [Inject] private JSInteropService JSInteropService { get; set; }
 
     #endregion
 
-    protected override void OnInitialized() =>
+    protected override void OnInitialized()
+    {
+        JSRuntimeService.AssociateJSRuntime(JSRuntime);
         NavigationService.AssociateNavigtionManager(NavigationManager);
+    }
 
     protected override async Task OnAfterRenderAsync(bool firstRender)
     {
