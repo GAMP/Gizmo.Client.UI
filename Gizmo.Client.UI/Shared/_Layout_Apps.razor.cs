6818d780709069072caa5238cb15ff9166e3cb8b
klapsis-netprojects <klapsis@netprojects.gr>
2023-05-22 10:30:37 +0300

    Create layout for home and apps.

---
+++ Gizmo.Client.UI/Shared/_Layout_Apps.razor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.Extensions.Options;
+using Microsoft.JSInterop;
+using System.Threading.Tasks;
+
+namespace Gizmo.Client.UI.Shared
+{
+    public partial class _Layout_Apps : LayoutComponentBase
+    {
+        [Inject]
+        IOptions<ClientUIOptions> ClientUIOptions { get; set; }
+
+        [Inject]
+        IJSRuntime JsRuntime { get; set; }
+
+        private async Task OnMainClick(MouseEventArgs e)
+        {
+            await JsRuntime.InvokeVoidAsync("closeOpenPopups", e);
+        }
+    }
+}
