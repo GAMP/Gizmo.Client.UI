aee5d0a236edd7a4311b94bea7fd72cf0298aa8a
klapsis-netprojects <klapsis@netprojects.gr>
2023-05-29 14:30:20 +0300

    Updates.

---
+++ Gizmo.Client.UI/Components/Shop/ProductSimpleCardHover.razor.cs
@@ -0,0 +1,37 @@
+﻿using Gizmo.Client.UI.View.States;
+using Gizmo.UI.Services;
+using System.Threading.Tasks;
+using Gizmo.Web.Components;
+using Microsoft.AspNetCore.Components;
+
+namespace Gizmo.Client.UI.Components
+{
+    public partial class ProductSimpleCardHover : CustomDOMComponentBase
+    {
+        [Inject]
+        ILocalizationService LocalizationService { get; set; }
+
+        [Parameter]
+        public UserProductViewState Product { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            if (Product != null)
+            {
+                this.SubscribeChange(Product);
+            }
+
+            await base.OnInitializedAsync();
+        }
+
+        public override void Dispose()
+        {
+            if (Product != null)
+            {
+                this.UnsubscribeChange(Product);
+            }
+
+            base.Dispose();
+        }
+    }
+}
