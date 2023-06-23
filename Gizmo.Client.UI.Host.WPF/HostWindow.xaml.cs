04aa3637162e6b9de63052b0580658b1990c2178
Oleg Savelos <olegsavelos@gmail.com>
2023-06-23 10:51:48 +0300

    Updates.

--- Gizmo.Client.UI.Host.WPF/HostWindow.xaml.cs
+++ Gizmo.Client.UI.Host.WPF/HostWindow.xaml.cs
@@ -1,5 +1,11 @@
 ﻿using Gizmo.Client.UI.Services;
 using Gizmo.UI;
+using Microsoft.AspNetCore.Components.WebView;
+using Microsoft.AspNetCore.Components.WebView.Wpf;
+using Microsoft.Web.WebView2.Core;
+using Microsoft.Web.WebView2.WinForms;
+using Microsoft.Web.WebView2.Wpf;
+using System;
 using System.IO;
 using System.Windows;
 
@@ -8,7 +14,7 @@ namespace Gizmo.Client.UI.Host.WPF
     /// <summary>
     /// Interaction logic for HostWindow.xaml
     /// </summary>
-    public partial class HostWindow : Window , IHostWindow
+    public partial class HostWindow : Window, IHostWindow
     {
         public HostWindow(DesktopUICompositionService componentDiscoveryService)
         {
@@ -26,13 +32,23 @@ namespace Gizmo.Client.UI.Host.WPF
         private void UpdateValues(DesktopUICompositionService componentDiscoveryService)
         {
             var rootComponent = componentDiscoveryService.RootComponentType;
-            if(rootComponent!=null)
+            if (rootComponent != null)
             {
                 //set component type based on the settings found by discovery service
                 _ROOT_COMPONENT.ComponentType = rootComponent;
-                 
+
                 _BLAZOR_WEB_VIEW.HostPage = Path.Combine(componentDiscoveryService.BasePath, @"wwwroot\index.html");
-            }           
+            }
+        }
+
+        private void BlazorWebViewInit(object sender, BlazorWebViewInitializedEventArgs e)
+        {
+            var staticFiles = Path.Combine(Environment.CurrentDirectory, "static");
+            if (Directory.Exists(staticFiles))
+            {
+                //map static folder
+                _BLAZOR_WEB_VIEW.WebView.CoreWebView2.SetVirtualHostNameToFolderMapping("static", staticFiles, CoreWebView2HostResourceAccessKind.Allow);
+            }
         }
     }
 }
