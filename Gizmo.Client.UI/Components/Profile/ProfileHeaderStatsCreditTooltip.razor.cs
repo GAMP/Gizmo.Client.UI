955efcda5bb22ae8be26ae4cf4ae05c7d9054ebf
klapsis-netprojects <klapsis@netprojects.gr>
2023-09-01 14:53:43 +0300

    Updates.

--- Gizmo.Client.UI/Components/Profile/ProfileHeaderStatsCreditTooltip.razor.cs
+++ Gizmo.Client.UI/Components/Profile/ProfileHeaderStatsCreditTooltip.razor.cs
@@ -1,8 +1,12 @@
-﻿using Gizmo.Web.Components;
+﻿using Gizmo.UI.Services;
+using Gizmo.Web.Components;
+using Microsoft.AspNetCore.Components;
 
 namespace Gizmo.Client.UI.Components
 {
     public partial class ProfileHeaderStatsCreditTooltip : CustomDOMComponentBase
     {
+        [Inject]
+        ILocalizationService LocalizationService { get; set; }
     }
 }
