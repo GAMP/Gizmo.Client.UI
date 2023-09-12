c014fcc9fcfddecfee01109c803a23e26b042993
klapsis-netprojects <klapsis@netprojects.gr>
2023-09-12 16:20:16 +0300

    Updates.

--- Gizmo.Client.UI/Pages/Profile/ProductsProductProperties.razor.cs
+++ Gizmo.Client.UI/Pages/Profile/ProductsProductProperties.razor.cs
@@ -5,6 +5,7 @@ using Gizmo.Web.Components;
 using Microsoft.AspNetCore.Components;
 using System;
 using Gizmo.UI.Services;
+using System.Reflection.Metadata.Ecma335;
 
 namespace Gizmo.Client.UI.Pages
 {
@@ -29,7 +30,46 @@ namespace Gizmo.Client.UI.Pages
         public int ProductId { get; set; }
 
         [Parameter]
+        public DateTime? PurchaseTime { get; set; }
+
+        [Parameter]
         public DateTime? FirstUsageTime { get; set; }
+
+        private string GetTime()
+        {
+            string result = string.Empty;
+
+            if (_userProductViewState != null && _userProductViewState.ProductType == ProductType.ProductTime)
+            {
+                if (_userProductViewState.TimeProduct.ExpirationOptions.HasFlag(ProductTimeExpirationOptionType.ExpireAfterTime))
+                {
+                    string time = string.Empty;
+
+                    switch (_userProductViewState.TimeProduct.ExpireAfterType)
+                    {
+                        case ExpireAfterType.Day:
+                            time = LocalizationService.GetString("GIZ_PRODUCT_TIME_EXPIRATION_DAYS_ABBREVIATED");
+                            break;
+                        case ExpireAfterType.Hour:
+                            time = LocalizationService.GetString("GIZ_PRODUCT_TIME_EXPIRATION_HOURS_ABBREVIATED");
+                            break;
+                        case ExpireAfterType.Minute:
+                            time = LocalizationService.GetString("GIZ_PRODUCT_TIME_EXPIRATION_MINUTES_ABBREVIATED");
+                            break;
+                    }
+
+                    result = $"{_userProductViewState.TimeProduct.ExpiresAfter} {time}";
+                }
+                if (_userProductViewState.TimeProduct.ExpirationOptions.HasFlag(ProductTimeExpirationOptionType.ExpireAtDayTime))
+                {
+                    DateTime dateTime = new DateTime();
+                    dateTime = dateTime.AddMinutes(_userProductViewState.TimeProduct.ExpireAtDayTimeMinute);
+                    result = $"{dateTime.ToLongTimeString()}";
+                }
+            }
+
+            return result;
+        }
 
         protected override async Task OnInitializedAsync()
         {
