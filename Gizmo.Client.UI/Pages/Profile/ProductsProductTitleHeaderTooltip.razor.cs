8a684e9aa2918ab1c3ff70a4e342e69b3d36d60d
klapsis-netprojects <klapsis@netprojects.gr>
2023-10-27 12:09:18 +0300

    Updates.

---
+++ Gizmo.Client.UI/Pages/Profile/ProductsProductTitleHeaderTooltip.razor.cs
@@ -0,0 +1,26 @@
+﻿using Gizmo.Web.Components;
+using Microsoft.AspNetCore.Components;
+using Gizmo.UI.Services;
+using System;
+using Gizmo.Client.UI.Components;
+using Gizmo.Client.UI.View.States;
+
+namespace Gizmo.Client.UI.Pages
+{
+    public partial class ProductsProductTitleHeaderTooltip : CustomDOMComponentBase
+    {
+        [Inject]
+        ILocalizationService LocalizationService { get; set; }
+
+        [Inject]
+        CreditOptionsViewState CreditOptionsViewState { get; set; }
+
+        [CascadingParameter]
+        protected ClientPopup ClientPopup { get; set; }
+
+        private void Close()
+        {
+            ClientPopup.Close();
+        }
+    }
+}
