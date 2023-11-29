68b1b4789bc6ab5af0cc43be2e514816a829b239
klapsis-netprojects <klapsis@netprojects.gr>
2023-11-29 15:54:26 +0200

    Add user ban message screen.

---
+++ Gizmo.Client.UI/Shared/UserBan.razor.cs
@@ -0,0 +1,34 @@
+﻿using Gizmo.Client.UI.View.Services;
+using Gizmo.Client.UI.View.States;
+using Gizmo.UI.Services;
+using Gizmo.Web.Components;
+using Microsoft.AspNetCore.Components;
+
+namespace Gizmo.Client.UI.Shared
+{
+    public partial class UserBan : CustomDOMComponentBase
+    {
+        [Inject]
+        ILocalizationService LocalizationService { get; set; }
+
+        [Inject]
+        UserBanViewState ViewState { get; set; }
+
+        [Inject]
+        UserViewService UserService { get; set; }
+
+        protected override void OnInitialized()
+        {
+            this.SubscribeChange(ViewState);
+
+            base.OnInitialized();
+        }
+
+        public override void Dispose()
+        {
+            this.UnsubscribeChange(ViewState);
+
+            base.Dispose();
+        }
+    }
+}
