e2965e5b201bc278872b615a67abc74c52eb90bc
mrdpod <mr.d.pod@mail.ru>
2026-09-24 12:49:13 +0300

    feat: ladder awaiting, frozen, projected rank and points breakdown

--- Gizmo.Client.UI/Components/Profile/LadderStandingCard.razor.cs
+++ Gizmo.Client.UI/Components/Profile/LadderStandingCard.razor.cs
@@ -1,12 +1,19 @@
+using System.Threading.Tasks;
 using Gizmo.Client.UI.View.States;
 using Gizmo.UI.Services;
 using Gizmo.Web.Components;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.JSInterop;
 
 namespace Gizmo.Client.UI.Components
 {
     public partial class LadderStandingCard : CustomDOMComponentBase
     {
+        private const string HistoryPopupSelector = ".giz-ladder-history-wrapper";
+
+        private bool _isBreakdownOpen;
+
         [Inject]
         ILocalizationService LocalizationService { get; set; }
 
@@ -18,6 +25,18 @@ namespace Gizmo.Client.UI.Components
             this.SubscribeChange(ViewState);
 
             base.OnInitialized();
+        }
+
+        private async Task OnBreakdownClick(MouseEventArgs e)
+        {
+            if (_isBreakdownOpen)
+            {
+                _isBreakdownOpen = false;
+                return;
+            }
+
+            await JsRuntime.InvokeVoidAsync("closeOpenPopups", e, HistoryPopupSelector);
+            _isBreakdownOpen = true;
         }
 
         public override void Dispose()
